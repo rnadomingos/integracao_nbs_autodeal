4390090d626f2852fa5442534214150891f061cb
Renan Domingos <rna.domingos@hotmail.com>
2025-06-19 09:26:11 -0300

    reafactor: move headers login to own function

--- api_autodeal.py
+++ api_autodeal.py
@@ -14,33 +14,45 @@ with open('dados.json', 'r', encoding='utf-8') as f:
 # CONVERT VARIABLE TO JSON DUMP 
 send_json = json.dumps(json_load)
 
-# MAKE HEADER TO LOGON
-headers_login = {'Content-Type':'application/x-www-form-urlencoded'}
-payload = {
-  'username': USERLOGIN,
-  'password': USERPASSWORD,
-  'grant_type': GRANT_TYPE
-}
 
 # GEN TOKEN TO ACCESS FUNCTION 
 def get_token_acesso():
     '''
     Function to receive token
     '''
+    # MAKE HEADER TO LOGON
+    headers_login = {'Content-Type':'application/x-www-form-urlencoded'}
+    payload = {
+      'username': USERLOGIN,
+      'password': USERPASSWORD,
+      'grant_type': GRANT_TYPE
+    }
+
     response = req.post(f'{URL_API}/api/token', data=payload, headers=headers_login)
     token = response.json()
     return token['access_token']
 
-# ASSIGN TOKEN
-token = get_token_acesso()
+def create_process():
+    '''
+    Function to create a new process
+    '''
+    # ASSIGN TOKEN
+    token = get_token_acesso()
 
-# MAKE HEADER TO SEND CREATE PROCESS IN AUTO DEAL
-headers_process = {'Content-Type':'application/json', 'accept':'*/*', "authorization":f'bearer {token}'}
+    # MAKE HEADER TO SEND CREATE PROCESS IN AUTO DEAL
+    headers_process = {'Content-Type':'application/json', 'accept':'*/*', "authorization":f'bearer {token}'}
 
-#SEND DATA TO CREATE PROCESS
-response_teste = req.post(f'{URL_API}/api/instance/start', data=send_json, headers=headers_process )
+    #SEND DATA TO CREATE PROCESS
+    response_teste = req.post(f'{URL_API}/api/instance/start', data=send_json, headers=headers_process )
 
-#PRINT RESULT
-print('Return: ', response_teste.json(), 'status code:', response_teste.status_code)
+    #PRINT RESULT
+    process = {
+        'body': response_teste.json(), 
+        'status code': response_teste.status_code
+    }
+    print('retorno: ', process)
+    return process
 
-#if __name__=='__main__': 
+if __name__=='__main__': 
+
+  create_process()
